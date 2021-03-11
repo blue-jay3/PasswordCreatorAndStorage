d5bd779ac51bb97c3e06e5d43af70a286f461cbb
whimsicalwisp <emferra01@gmail.com>
2021-03-11 10:52:50 -0500

    Commit on master

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
         showGUI startApp = new showGUI();
-        startApp.gui();
+
     }
 }
 
--- src/com/example/showGUI.java
+++ src/com/example/showGUI.java
@@ -1,18 +1,41 @@
 package com.example;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class showGUI {
-    JFrame myFrame = new JFrame("Password Generator");
+public class showGUI implements ActionListener {
+    JFrame myFrame;
     passwordCreator pwd = new passwordCreator();
 
-    public void gui(){
+    public showGUI(){
+        myFrame = new JFrame("Password Generator");
         JButton btn = new JButton("Generate");
+        JTextField pwdLength = new JTextField(7);
+        JTextField pwdNumber = new JTextField(7);
 
         myFrame.setLayout(null);
         myFrame.setSize(700, 400);
         myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 
+        btn.setSize(130,70);
+        btn.setLocation(270,200);
+        btn.setBackground(Color.LIGHT_GRAY);
 
+        pwdLength.setLocation(30, 60);
+        pwdLength.setLocation(70, 60);
+
+        myFrame.add(btn);
+        myFrame.add(pwdLength);
+        myFrame.add(pwdNumber);
+
+        btn.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(pwd.randomizer());
+        System.out.println(pwd.randomizer());
+        System.out.println(pwd.randomizer());
     }
 }
